d2653bb85dcee2cfbf71a738fd8bcad65057120b
brendanvanbreda <brendanvanbreda@users.noreply.github.com>
2017-03-01 13:23:23 +0200

    Update SW01_Temperature_Measurement.ino

--- examples/SW01_Temperature_Measurement/SW01_Temperature_Measurement.ino
+++ examples/SW01_Temperature_Measurement/SW01_Temperature_Measurement.ino
@@ -3,36 +3,64 @@
 	Digital Temperature, Pressure and Humidity Sensor
 	
 	You can buy one on our store!
-	-----> https://xinabox.cc/modules/sensors/SW01/
+	-----> https://xinabox.cc/SW01/
 	
 	This example request a temperature meausrement to be 
-	made by the SW01 ad display the data over the Serial
+	made by the SW01 and display the data over the Serial
 	bus.
+	
+	Currently Supported on the following ☒CHIP:
+	- CW01
+	- CC01
+	- CR01
 	
 	The sensor communicates over the I2C Bus.
 	
+	------------------------TIPS--------------------------
+	Change this line ----->Wire.begin(2,14);
+	to this			 ----->Wire.begin();
+	to allow this sensor to communicate with CC01 and CR01
+	
 *************************************************************/
 
-#include <XiX_Core.h>
-#include <XiX_SW01.h>
+#include <xCore.h>
+#include <xSW01.h>
 
-const int DELAY_TIME 1000
+const int DELAY_TIME = 1000;
 
 void setup(){
-	Serial.begin(9600);
+	// Start the Serial Monitor
+	Serial.begin(115200);
+	
+	// Start the I2C Comunication
+	Wire.begin(2,14);
+	
+	// Start the  SW01 Sensor
 	SW01.begin();
+	
 	Serial.println("================================");
-	Serial.println("=   XINABOX SW01 Temperature   =");
+	Serial.println("    XINABOX SW01 Temperature    ");
 	Serial.println("================================");
 	delay(DELAY_TIME);
 }
 
 void loop(){
-	int value;
-	value = 0;
-	value = SW01.getAltitude();
-	Serial.println("Temperature: ")
-	Serial.print(value,2);
-	Serial.print("*C");
+	// Create a variable to store the data read from SW03	
+	float temperature;
+	temperature = 0;
+  
+  	// Read and calculate data from SW01 sensor
+  	SW01.poll();
+  
+	// Request SW01 to get the temperature measurement and store in
+	// the temperature variable		
+	temperature = SW01.getTemperature_C();
+	
+	// Display the recoreded data over the Serial Monitor		
+	Serial.print("Temperature: ");
+	Serial.print(temperature);
+	Serial.println(" *C");
+
+  	// Small delay between sensor reads
 	delay(DELAY_TIME);
 }
